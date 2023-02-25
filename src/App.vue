e32b6a4fa1d80b31ed051bf06ab14cc452a505f8
LaurenV <laurenvdl45@gmail.com>
2023-02-25 10:14:56 +0100

    feat: start navigation and add logo

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+//import router view
+import { useRouter, useRoute } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <nav>
+        <img src="../public/images/Logo.png" alt="Ziezo Logo">
+    </nav>
+    <RouterView></RouterView>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
