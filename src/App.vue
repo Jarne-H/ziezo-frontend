f9e8e7745896e94450702c52db59096b54417b26
LaurenV <laurenvdl45@gmail.com>
2023-02-25 11:31:40 +0100

    style: add hover effects

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,45 @@ ul {
   text-align: left;
 }
 
+li {
+  font-family: 'Figtree';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  gap: 12px;
+  display: flex;
+  align-items: center;
+  color: #172230;
+  flex: none;
+  order: 1;
+  flex-grow: 1;
+  margin-bottom: 12px;
+}
+
+li:hover {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 4px 16px;
+  gap: 12px;
+
+  position: static;
+  width: 180px;
+  height: 40px;
+  left: 20px;
+  top: 80px;
+
+  background: #ECECEF;
+  border-radius: 20px;
+}
+
+li:active {
+  background: #EF7946;
+  opacity: 0.2;
+  color: #2A1005;
+}
+
 .nav-logo {
   width: 100%;
   height: 100%;
@@ -60,29 +99,22 @@ ul {
 
 .nav-menu {
   position: relative;
-  top: -304px;
+  top: -204px;
   width: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: left;
   gap: 4px;
-  width: 180px;
-  height: 172px;
   order: 1;
   flex-grow: 0;
 }
 
 .nav-details {
   position: relative;
-  left: 0;
-  top: -100px;
+  top: -104px;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  padding: 0px;
-  width: 179px;
-  height: 108px;
-  flex: none;
+  align-items: left;
   order: 2;
   flex-grow: 0;
 }
